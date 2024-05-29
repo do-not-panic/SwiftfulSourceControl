449e7e9706536d33fc9a74c760dda7716a6525e6
René <schach@gmail.com>
2024-05-29 13:52:30 +0200

    [Feature] add Rectangle to ContentView, tweaks to HomeView

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -7,20 +7,18 @@
 
 import SwiftUI
 
+
 struct HomeView: View {
     
     @State private var title: String = ""
     
     var body: some View {
-        ZStack {
+        VStack {
+            Text("Hi")
+            Text("Screen 2!")
+            
             Text("Screen 2!")
             Text("Screen 2!")
-            Text("Screen 2!")
-            VStack {
-                Text("Screen 2!")
-                Text("Screen 2!")
-                Text("Screen 2!")
-            }
         }
         
     }
